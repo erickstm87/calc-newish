a9a643becc35abc5fd52193190068dfdcca5e4d8
erickstm87 <dev@terickson.io>
2018-10-22 13:10:32 -0600

    setup component file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {
+    Input
+} from './components/'
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-import App from './App';
-import style from './main.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
